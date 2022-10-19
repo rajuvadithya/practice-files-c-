560aaed226dccd1d4b2411298e01304880264daa
rajuvadithya <115393589+rajuvadithya@users.noreply.github.com>
2022-10-19 09:45:06 +0530

    program-1, program-2, program-3.

---
+++ c#/FirstApp.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Data;
+
+namespace FirstApp
+
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+
+
+        {
+            
+            {
+                //  Console.WriteLine("HelloWorld");
+                //  Console.ReadLine();
+
+
+                //int a, b, c;
+                //a = 10;
+                //b = 20;
+                //c = a + b;
+                //Console.WriteLine("The Sum Of {0} and {1} is {2}",a,b,c);
+
+
+                //int a, b, c;
+                //a = int.Parse(Console.ReadLine());
+                //b = int.Parse("50");
+                //c = a + b;
+                //Console.WriteLine("The Sum Of {0} and {1} is {2}",a,b,c); 
+            }
+
+
+            //int a = 10;
+            //int b = 20;
+            //if (a > b)
+            //{
+            //    Console.WriteLine("{0} is Greater", a);
+
+            //}    
+            //else
+            //{
+            //    Console.WriteLine("{0} is Greater",b);
+            //}
+
+
+
+            //int a = 10;
+            //int b = 20;
+            //int c = 30;
+            //if (a > b && a > c)
+            //{
+            //    Console.WriteLine("{0} is Greater", a);
+
+            //}
+            //else
+            //{
+            //    if (b > a && b > c)
+            //    {
+            //        Console.WriteLine("{0} is Greater", b);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(("{0} is Greater", c);
+            //    }
+            //}
+
+
+
+            //    int a = 10;
+            //    int b = 20;
+            //    int opt;
+
+            //L: Console.WriteLine("1.Add 2.Mul 3.Sub 4.Exit");
+            //    opt = int.Parse(Console.ReadLine());
+
+            //    switch (opt)
+            //    {
+            //        case 1:
+            //            Console.WriteLine("Addition is " + (a + b));
+            //            break;
+            //        case 2:
+            //            Console.WriteLine("multiplication is " + (a * b));
+            //            break;
+            //        case 3:
+            //            Console.WriteLine("sub is " + (a - b));
+            //            break;
+            //        case 4:
+            //            break;
+            //        default:
+            //            Console.WriteLine("Invalid Option !");
+            //        goto L; ;
+
+
+
+
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Raju");
+            //}
+
+            ////6 * 1 = 6
+            ////6 * 2 = 12
+            //int n = 6;
+            //for (int i = 1; i <=10; i++)
+            //{
+            //    Console.WriteLine("{0}  *  {1}  =  {2}", n, i, n * i);
+            //}
+            //Console.WriteLine("----------");
+            ////int n = 6;
+            //for (int i = 10; i >=1; i--)
+            //{
+            //    Console.WriteLine("{0}  *  {1}  =  {2}", n, i, n * i);
+            //}
+
+            //int n = 236457;
+            //sum=10
+            //r=n%10=7
+            //sum =sum +r=7
+            //n=n/10=236
+
+            //r=n%10=6
+            //sum =sum +r=13
+            //n=n/10=23
+
+            //r=n%10=3
+            //sum =sum +r=16
+            //n=n/10=2
+
+            //r=n%10=2
+            //sum =sum +r=18
+            //n=n/10=0
+
+            //int sum = 0;
+            //int r = 0;
+            //while (n!=0)
+            //{
+            //    r = n % 10;
+            //    sum = sum + r;
+            //    n = n / 10;
+            //}
+            //Console.WriteLine("Sum is "+sum);
+            //int i;
+            //do
+            //{
+            //    i = int.Parse(Console.ReadLine());
+            //} while (i!=0);
+
+            //for(; ; )
+            //{
+            //    Console.Write("Raju");
+            //}
+
+            //while(true)
+            //{
+            //    Console.Write("Raju"); 
+            //} 
+
+
+
+            #region Arrays
+            // Console.ReadLine();
+
+            //int[] myArray = new int[5];
+            //myArray[0] = 78;
+            //myArray[1] = myArray[2] = 34;
+            //myArray[3] = myArray[0] + myArray[1];
+            //myArray[4] = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter Values:");
+            //for (int i = 0; i < myArray.Length; i++)
+            //{
+            //    myArray[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("Values are:");
+            //for(int i=0;i<myArray.Length;i++)
+            //{
+            //    Console.WriteLine(myArray[i]);
+            //}
+
+            //Console.WriteLine("Values Using foreach");
+            //foreach(int k in myArray)
+            //{
+            //    Console.WriteLine(k);
+            //}
+
+            //Console.WriteLine("Values Using foreach >100");
+            //foreach (int k in myArray)
+            //{
+            //    if(k>100)
+            //    Console.WriteLine(k);
+            //}
+
+            //Sum of all the elements
+            //int sum = 0;
+            //foreach (var item in myArray)
+            //{
+            //    sum = sum + item; //sum += item;
+            //}
+            //Console.WriteLine("Sum of all the elements is " + sum);
+
+
+            //Largest element
+            //int largest = myArray[0];
+            //foreach (var item in myArray)
+            //{
+            //    if(item>largest)
+            //    {
+            //        largest = item;
+            //    }
+            //}
+            //Console.WriteLine("Largest is " + largest);
+
+            //Array.Sort(myArray);
+            //Array.Reverse(myArray);
+            //Console.WriteLine("Elements after sorting");
+            //foreach (var item in myArray)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //int index = Array.IndexOf(myArray, 100);
+            //Console.WriteLine("Position of Element is "+(index+1));
+
+            //string[,] Contacts = new string[3, 2];
+            //Contacts[0, 0] = "Peter";
+            //Contacts[0, 1] = "1234567890";
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 2; j++)
+            //    {
+            //        Contacts[i, j] = Console.ReadLine();
+            //    }
+            //}
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 2; j++)
+            //    {
+            //        Console.Write(Contacts[i, j]+ "  ");                    
+            //    }
+            //    Console.WriteLine();
+            //} 
+            #endregion
+
+            #region Struct
+            //Student S;
+            //S.Sid = 100;
+            //S.Name = "Raju";
+            //S.Avg = 89.89;
+
+            //Console.WriteLine("Sid : {0} Name:{1} Avg:{2}",S.Sid,S.Name,S.Avg);
+
+
+            //Student[] S = new Student[3];
+            //for (int i = 0; i < S.Length; i++)
+            //{
+            //    Console.WriteLine("Enter {0} Record",(i+1));
+            //    S[i].Sid = int.Parse(Console.ReadLine());
+            //    S[i].Name = Console.ReadLine();
+            //    S[i].Avg = double.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("All Students");
+            //foreach (var item in S)
+            //{
+            //    Console.WriteLine("Sid : {0} Name:{1} Avg:{2}", item.Sid, item.Name, item.Avg);
+            //}
+
+            //Console.WriteLine("1st Division students");
+            //foreach (var item in S)
+            //{
+            //    if (item.Avg >= 60)
+            //        Console.WriteLine("Sid : {0} Name:{1} Avg:{2}", item.Sid, item.Name, item.Avg);
+            //} 
+            #endregion
+
+            #region var Vs dynamic
+            //int i;
+            //i = 10;
+
+            //var i = 10;
+            //i = "Raju";
+
+            //dynamic x;
+
+            //x = "Raju";
+            //Console.WriteLine(x);
+
+            //x = 23.8;
+            //Console.WriteLine(x);
+
+            //x = S;
+            //Console.WriteLine("Sid : {0} Name:{1} Avg:{2}", x.Sid, x.Name, x.Avg); 
+            #endregion
+
+
+
+
+         
+            Console.ReadLine();
+
+
+
+        }
+
+        
+    }
+}
+
+   
---
+++ c#/Program 2.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Data;
+
+namespace OOPs1
+{
+    #region Class,Feilds,Methods And Objects
+    //class Student
+    //{
+    //    int Sid;
+    //    string Name;
+    //    double M1, M2;
+
+    //    public void CreateStudent(int id,string name)
+    //    {
+    //        Sid = id;
+    //        Name = name;
+    //    }
+
+    //    public void SetMarks(double m1,double m2)
+    //    {
+    //        M1 = m1;
+    //        M2 = m2;
+    //    }
+
+    //    public void Result()
+    //    {
+    //        double avg = (M1 + M2) / 2;
+    //        if(avg>=35)
+    //        {
+    //            Console.WriteLine("Pass");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Fail");
+    //        }
+    //    }
+    //}
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Student S;
+    //        S = new Student();
+
+    //        Console.WriteLine("Enter Sid And Name");
+    //        int id = int.Parse(Console.ReadLine());
+    //        string name = Console.ReadLine();
+
+    //        S.CreateStudent(id, name); //Initialize Object Explicitly - issue 1
+
+    //        Console.WriteLine("Enter Marks 1 and 2");
+    //        double m1 = double.Parse(Console.ReadLine());
+    //        double m2 = double.Parse(Console.ReadLine());
+
+    //        S.SetMarks(m1, m2);
+
+    //        S.Result();
+
+    //        S.CreateStudent(200, "raju"); //Re-initialize the object - issue 2
+
+
+    //        //Student S1 = new Student();
+    //        //S1.CreateStudent(200, "raju");
+
+    //       Console.ReadLine();
+
+
+    //    }
+    //} 
+    #endregion
+
+    #region Constructors
+    //class Student
+    //{
+    //    int Sid;
+    //    string Name;
+    //    double M1, M2;
+
+    //    public Student()
+    //    {
+    //        Console.WriteLine("This is default Constructor");
+    //    }
+
+    //    public Student(int id, string name)
+    //    {
+    //        Sid = id;
+    //        Name = name;
+    //    }
+
+    //    public void SetMarks(double m1, double m2)
+    //    {
+    //        M1 = m1;
+    //        M2 = m2;
+    //    }
+
+    //    public void Result()
+    //    {
+    //        double avg = (M1 + M2) / 2;
+    //        if (avg >= 35)
+    //        {
+    //            Console.WriteLine("Pass");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Fail");
+    //        }
+    //    }
+    //}
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Student S1 = new Student(200,"raju");
+
+
+    //        Student S = new Student(100,"Peter");
+
+    //        S.SetMarks(23, 45);
+
+    //        S.Result();
+
+    //        //S.Student(200, "raju");
+
+    //        Console.ReadLine();
+    //    }
+    //} 
+    #endregion
+
+
+    #region Static-Feilds,Constructors,Properties & Methods
+    //class HousingLoanCustomer
+    //{
+    //    int CustomerId;
+    //    string Name;
+    //    double LoanAmount;
+    //    static double _ROI;
+
+    //    public static double ROI
+    //    {
+    //        get { return _ROI; }
+    //        set { _ROI = value; }
+    //    }
+
+    //    static HousingLoanCustomer()
+    //    {
+    //        _ROI = 11.2;
+    //    }
+
+    //    public HousingLoanCustomer(int CustomerId, string Name, double LoanAmount)
+    //    {
+    //        this.CustomerId = CustomerId;
+    //        this.Name = Name;
+    //        this.LoanAmount = LoanAmount;
+    //    }
+
+    //    public void CalculateIntrest(int months)
+    //    {
+    //        double IntAmount = LoanAmount * _ROI / 100;
+    //        double RePayAmount = LoanAmount + IntAmount;
+    //        Console.WriteLine("Loan Amount:{0} IntAmount:{1} RePayAmount:{2} ROI:{3}", LoanAmount, IntAmount, RePayAmount, _ROI);
+    //        Console.WriteLine("EMI:" + (RePayAmount / months));
+    //    }
+
+    //    public static void Enquiry(double EnLoanAmount, int months)
+    //    {
+    //        Console.WriteLine("ROI :" + ROI);
+    //        Console.WriteLine("Loan Amount:" + EnLoanAmount);
+    //        Console.WriteLine("IntAmount:" + EnLoanAmount * ROI / 100);
+    //        Console.WriteLine("RepayAmount:" + (EnLoanAmount + (EnLoanAmount * ROI / 100)));
+    //        Console.WriteLine("EMI Months:" + months);
+    //        Console.WriteLine("EMIs:" + (EnLoanAmount + (EnLoanAmount * ROI / 100)) / months);
+    //    }
+    //}
+    //class Program
+    //{      
+    //    static void Main(string[] args)
+    //    {
+
+    //        var loanCustomer1 = new HousingLoanCustomer(1, "Peter", 75000);
+    //        loanCustomer1.CalculateIntrest(12);
+
+    //        var loanCustomer2 = new HousingLoanCustomer(2, "Jack", 85000);
+    //        loanCustomer2.CalculateIntrest(24);
+
+    //        HousingLoanCustomer.ROI = 11.5;
+
+    //        loanCustomer1.CalculateIntrest(12);
+    //        loanCustomer2.CalculateIntrest(24);
+
+    //        HousingLoanCustomer.Enquiry(60000, 24);
+
+    //        //var loanCustomer3 = new HousingLoanCustomer(3, "Lily", 175000);
+    //        //var loanCustomer4 = new HousingLoanCustomer(4, "Bob", 80000);
+
+    //        Console.ReadLine();
+    //    }
+    //} 
+    #endregion
+
+    #region Static - Class
+    //static class Calculation
+    //{
+    //    public static void Add(int a,int b)
+    //    {
+    //        Console.WriteLine("Additin is " + (a + b));
+    //    }
+    //    public static void Multiply(int a, int b)
+    //    {
+    //        Console.WriteLine("multiplication is " + (a * b));
+    //    }
+    //}
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        //Calculation C;
+    //        //C = new Calculation();
+
+    //        Calculation.Add(23, 45);
+    //        Calculation.Multiply(34, 67);
+    //        Console.ReadLine();
+    //    }
+    //} 
+    #endregion
+
+
+    using System;
+
+    #region InheritanceEg
+    //namespace InheritanceEg1
+    //{
+    //    class Customer
+    //    {
+    //        public int CustomerId { get; set; }
+    //        public string Name { get; set; }
+    //        public string Email { get; set; }
+
+    //        public void Details()
+    //        {
+    //            Console.WriteLine("CustomerId" + CustomerId);
+    //        }
+    //    }
+
+    //    class BankCustomer:Customer
+    //    {
+    //        public int AccountNumber { get; set; }
+    //        public double Balance { get; set; }
+
+    //        public void BankCustomerDetails()
+    //        {
+    //            Console.WriteLine("CustomerId " + CustomerId);
+    //            Console.WriteLine("Bal " + Balance);
+    //        }
+    //    }
+
+    //    class LoanCustomer:BankCustomer
+    //    {
+    //        public double LoanAmount { get; set; }
+    //    }
+
+    //    class Program
+    //    {
+    //        static void Main(string[] args)
+    //        {
+    //            Customer C = new Customer()
+    //            {
+    //                CustomerId = 123,
+    //                Name = "raju",
+    //                Email = "rajuk5425@gmail.com"
+    //            };
+
+    //            BankCustomer BC = new BankCustomer()
+    //            {
+    //                CustomerId = 124,
+    //                Name = "Peter",
+    //                Email="Peter@gmail.com",
+    //                AccountNumber=12345,
+    //                Balance=5000
+    //            };
+
+    //            BC.Details();
+    //            BC.BankCustomerDetails();
+
+    //            C.Details();
+
+    //            LoanCustomer LC = new LoanCustomer();
+    //            //LC. Can access all the properties of Customer, 
+    //            //BankCustomer And LoanCustomer
+    //        }
+    //    }
+    //} 
+    #endregion
+
+    #region Protected Members
+    //namespace InheritanceEg1
+    //{
+    //    class Customer
+    //    {
+    //        protected int CustomerId;
+    //        string Name;
+
+    //        public void Details()
+    //        {
+    //            Console.WriteLine("CustomerId" + CustomerId);
+    //            Console.WriteLine("Name" + Name);
+    //        }
+    //    }
+
+    //    class BankCustomer:Customer
+    //    {
+    //        int AcNo;
+
+    //        public void BankCustomerDetails()
+    //        {
+    //           // CustomerId
+    //        }
+    //    }
+
+    //    class Program
+    //    {
+    //        static void Main(string[] args)
+    //        {
+    //            Customer C = new Customer();          
+
+    //            BankCustomer BC = new BankCustomer();            
+    //        }
+    //    }
+    //} 
+    #endregion
+
+    #region ConstructorChaining
+    //namespace InheritanceEg1
+    //{
+    //    class Customer
+    //    {
+    //        protected int CustomerId;
+    //        protected string Name;
+    //        protected string Email;
+
+    //        public Customer()
+    //        {
+    //            Console.WriteLine("This is Customer Constructor!");
+    //        }
+
+    //        public Customer(int CustomerId,string Name,string Email)
+    //        {
+    //            Console.WriteLine("This is Customer Constructor with Param");
+    //            this.CustomerId = CustomerId;
+    //            this.Name = Name;
+    //            this.Email = Email;
+    //        }
+    //    }
+
+    //    class BankCustomer : Customer
+    //    {
+    //        protected int AcNo;
+    //        protected double Balance;
+
+    //        public BankCustomer()
+    //        {
+    //            Console.WriteLine("This is Bank Customer Constructor");
+    //        }
+
+    //        public BankCustomer(int CustomerId, string Name, string Email,int AcNo,double Balance)
+    //            :base(CustomerId,Name,Email)
+    //        {
+    //            Console.WriteLine("This is Bank Customer Constructor with Param");
+    //            //this.CustomerId = CustomerId;
+    //            //this.Name = Name;
+    //            //this.Email = Email;
+    //            this.AcNo = AcNo;
+    //            this.Balance = Balance;
+    //        }
+    //    }
+
+    //    class Program
+    //    {
+    //        static void Main(string[] args)
+    //        {
+    //            var BC = new BankCustomer(123, "raju", "xxxx", 7678, 5000);
+
+    //        }
+    //    }
+    //} 
+    #endregion
+
+
+
+}
+
